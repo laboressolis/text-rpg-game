091774d37de9d8799e2dec7eafc9b6f782409c1a
icy <zheanis15@gmail.com>
2023-11-13 18:37:36 +0800

    Story Progression and plot finalization

--- basic_functions.py
+++ basic_functions.py
@@ -17,8 +17,16 @@ def typing(message):
     time.sleep(.1)
     return ""
 
+def fasttyping(message):
+    for letter in message:
+        time.sleep(random.choice([0.11, 0.01, 0.08, 0.01,0.01, 0.07, 0.06, 0.06, 0.05, 0.01]))
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+    time.sleep(.1)
+    return ''
+
 def speechbreak():
-    print("Press any key to continue....\n")
+    print("Press any key to continue....", end='')
     msvcrt.getch()
 
 def createcharacter():
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from basic_functions import typing, clear_screen, speechbreak
+from basic_functions import typing, fasttyping, clear_screen, speechbreak
 import time
 
 start_logo = r'''
@@ -20,4 +20,16 @@ def start():
     typing("The once peaceful kingdom is now threatened by the rise of an ancient evil force known as the Shadow Cult. \n")
     typing("As a skilled adventurer, you embark on a journey to unravel the secrets of Eldoria, facing perilous challenges and making crucial decisions that will shape the fate of the land. \n")
     speechbreak()
+    fasttyping("You arive at Celestria's main gate, the biggest city in Eldoria. \n")
+    fasttyping("You head towards the Adventurer's Guild in city center where you meet Oberon.\n")
+    typing("Oberon: You look new here son! What bring you here? Looking for some sweet money? \n")
+    typing("You: No, I'm just an Adventurer. \n")
+    typing("Oberon: Ohhh... So you're one of those kind, huh? \n")
+    typing("Oberon: Well to buy anything here at the guild you'd a guild emblem, you register there at the counter. \n")
+    typing("Oberon: This old man sells a lot of things. \n")
+    typing(' ')
+    fasttyping("You head over to the counter to register.")
+    clear_screen()
+    # begin character creation
+
 start()
