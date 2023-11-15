6212e31181cba2f07a4b2471dea808807e4a95e1
icy <zheanis15@gmail.com>
2023-11-15 15:21:21 +0800

    Added Player Status Menu

--- player_class.py
+++ player_class.py
@@ -16,13 +16,16 @@ class Player:
 
         self.max_health = 100
         self.health = self.max_health
+        self.health_regen = 10
 
         self.max_stamina = max_stamina
         self.stamina = self.max_stamina
+        self.stamina_regen = 10
 
         self.max_mana = max_mana
         self.mana = self.max_mana
-
+        self.mana_regen = 10
+        
         self.max_defense = max_defense
         self.defense = self.max_defense
 
@@ -35,6 +38,33 @@ class Player:
         # {EQUIPID: {id: value, type: string, name: string, attack: value, defense: value, class: string}}
         self.equipment_inventory = {1:{'id': 1, 'type': 'weapon', 'name': 'Excalibur','attack': 20, 'defense': 0, 'class': 'physical'}}
 
+    def player_status(self):
+        print(f"\n{'*' * 30}")
+        print(f"{'*' * 10} PLAYER STATUS {'*' * 10}")
+        print(f"{'*' * 30}\n")
+        
+        print(f"Player: {self.name}")
+        print(f"Class: {self.player_class}")
+        print(f"Level: {self.level}")
+        print(f"{'-' * 30}")
+        print(f"Health: {self.health}/{self.max_health} (+{self.health_regen} regen)")
+        print(f"Stamina: {self.stamina}/{self.max_stamina} (+{self.stamina_regen} regen)")
+        print(f"Mana: {self.mana}/{self.max_mana} (+{self.mana_regen} regen)")
+        print(f"Defense: {self.defense}/{self.max_defense}")
+        print(f"{'-' * 30}")
+        print(f"Base Attack: {self.base_attack}")
+        print(f"Coins: {self.coin}")
+        print(f"Skill Points: {self.skill_points}")
+        
+        print("\nEquipment:")
+        for slot, item in self.equipment.items():
+            print(f"  {slot.capitalize()}: {item}")
+        
+        print(f"\n{'*' * 30}\n")
+
+    def display_player_status(self):
+        print(f"")
+
     def display_equipment_inventory(self):
         print("Equipment Inventory:")
         for equipment_id, equipment in self.equipment_inventory.items():
@@ -46,3 +76,4 @@ class Player:
             print(f"{i}. {item} ({quantity})")
 
 # player = Player('icy','physical',50,50,50,20)
+
